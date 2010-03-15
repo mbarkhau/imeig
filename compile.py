e26f68a002652b874bed4120ee53c384c14a1afd
barkhau <barkhau@client43.rbg.informatik.tu-darmstadt.de>
2010-03-15 14:37:43 +0100

    metatdata parsing

--- compile.py
+++ compile.py
@@ -1,6 +1,14 @@
-#!/usr/bin/python2.6
+#!/usr/bin/python2.5
 
 import os 
+from ConfigParser import SafeConfigParser
+
+METAFILE_DEFAULT = "metadata.defaults"
+METAFILE = "metadata"
+
+config = SafeConfigParser()
+config.readfp(open(METAFILE_DEFAULT))
+config.read(METAFILE)
 
 def filelist():
     files = os.listdir(".")
@@ -10,10 +18,15 @@ def filelist():
 
 files = " ".join(filelist())
 
-print pdf_cmd
+refname     = config.get("info", "refname")
 
-html_cmd = "pandoc --toc -o IE%s.html %s" % (lang, files)
-#os.system(html_cmd)
+title       = config.get("info", "title")
+subtitle    = config.get("info", "subtitle")
+authors     = config.get("info", "authors").split(",")
+contribs    = config.get("info", "contributors").split(",")
+
+html_cmd = "pandoc --toc -o web/%s.html %s" % (refname, files)
+os.system(html_cmd)
 
 #pdf_cmd = "markdown2pdf --custom-header=header_%s.template --toc -o IE_%s.pdf %s" % (lang, lang, files)
 #os.system(pdf_cmd)
