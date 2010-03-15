a2b3f29f8ccdebac83754e021d019c5e4144a054
barkhau <barkhau@client43.rbg.informatik.tu-darmstadt.de>
2010-03-15 13:15:06 +0100

    only html output

--- compile.py
+++ compile.py
@@ -2,21 +2,18 @@
 
 import os 
 
-lang = "de"
-
-def filelist(lang="de"):
+def filelist():
     files = os.listdir(".")
-    files = filter(lambda f: "_" + lang + "_" in f, files)
     files = filter(lambda f: f.endswith(".mkd"), files)
     files.sort()
     return files
 
-files = " ".join(filelist(lang))
-
-html_cmd = "pandoc --toc -o IE_%s.html %s" % (lang, files)
-pdf_cmd = "markdown2pdf --custom-header=header_%s.template --toc -o IE_%s.pdf %s" % (lang, lang, files)
+files = " ".join(filelist())
 
 print pdf_cmd
 
-os.system(html_cmd)
-os.system(pdf_cmd)
+html_cmd = "pandoc --toc -o IE%s.html %s" % (lang, files)
+#os.system(html_cmd)
+
+#pdf_cmd = "markdown2pdf --custom-header=header_%s.template --toc -o IE_%s.pdf %s" % (lang, lang, files)
+#os.system(pdf_cmd)
