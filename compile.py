fd7795b7a0f14ade401e91f1f09fac212101bc1b
root <root@vserver1988.vserver-on.de>
2010-06-08 15:04:31 +0200

    Added: preliminary compile script

---
+++ compile.py
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+MARKBOOK='/home/koloss/Dropbox/projects/markbook/markbook.py' 
+
+BASE_DIR = /home/server/ie/web
+EN_DIR = /home/server/ie/web/en
+DE_DIR = /home/server/ie/web/de
+
+cd $EN_DIR
+cd $EN_DIR
+
+#/home/server/markbook/markbook.py
+
+#cd /home/server/ie/web/de
+#mv out.pdf IE.pdf
+#zip ie_quellen.zip *.mkd metadata part1/* part2/*
